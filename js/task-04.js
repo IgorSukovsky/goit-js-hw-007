e818dbdd1f2b12dd4e71f5e7c623d7577e488aa7
IgorSukovsky <Sukovsky70@gmail.com>
2021-08-19 15:59:54 +0300

    add-hw-07

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const refs = {
+  value: document.getElementById('value'),
+  increm: document.querySelector("[data-action='increment']"),
+  decrem: document.querySelector("[data-action='decrement']"),
+};
+let counterValue = 0;
+const updatValue = () => {
+  refs.value.textContent = counterValue;
+};
+
+const increment = () => {
+  counterValue += 1;
+  updatValue();
+};
+
+const decrement = () => {
+  counterValue -= 1;
+  updatValue();
+};
+refs.increm.addEventListener('click', increment);
+refs.decre.maddEventListener('click', decrement);
---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+let input = document.getElementById('name-input');
+let nameOutput = document.getElementById('name-output');
+
+input.oninput = function () {
+  let text = 'незнакомец';
+  if (input.value) text = input.value;
+  nameOutput.innerHTML = text;
+};
